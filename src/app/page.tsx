d086b9b1b46a9cb9a110a0ace2865c7210eb1188
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-01 23:55:13 +0500

    add new project in my Next js Journey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Navbar from "./components/Navbar/page"
 import HomePage from "./components/HomePage/page"
 import Group from "./components/Group/page"
+import Desination from "./components/Desination/page"
 export default function Home() {
   return (
     <>
     <Navbar/>
     <HomePage/>
     <Group/>
+    <Desination/>
 
     </>
   );
