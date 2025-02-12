ae9285af3b205e99572a7195dc2d5d30874f0c19
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-12 19:35:38 +0500

    Add new section Testonamils

---
+++ src/app/components/Testonails.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Testonails() {
+  return (
+    <div>
+        <div>
+            <h3>TESTIMONIALS</h3>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      
+    </div>
+  )
+}
+
+export default Testonails
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Group from "./components/Group/page"
 import Desination from "./components/Desination/page"
 import Footer from "./components/Footer";
 import EmailSection from "./components/EmailSection";
+import Testonails from "./components/Testonails";
 export default function Home() {
   return (
     <>
@@ -12,6 +13,7 @@ export default function Home() {
     <HomePage/>
     <Group/>
     <Desination/>
+    <Testonails/>
     <EmailSection/> 
     <Footer/>
 
